c064a5fdf06ef558d26ba2ec31f7f928c470fda8
Vital Rudkouski <Vital.Rudkouski@thomsonreuters.com>
2023-07-19 17:14:19 +0300

    test

--- auth/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm /etc/nginx/conf.d/default.conf || :
-envsubst < auth.conf > /etc/nginx/conf.d/auth.conf
-envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
-
-exec nginx -g "daemon off;"
